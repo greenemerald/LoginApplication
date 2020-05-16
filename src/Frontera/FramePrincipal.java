2ea0492e5780077dd72365fa692619c0a578e13b
Pink <Pink@DESKTOP-L8MJG2E.lan>
2020-05-16 13:11:00 -0500

    Lab2REalizado

--- src/Frontera/FramePrincipal.java
+++ src/Frontera/FramePrincipal.java
@@ -1,22 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Frontera;
 
-/**
- *
- * @author Pink
- */
+
+import Entidad.Sistema;
+import Entidad.Usuario;
+import java.util.ArrayList;
+
 public class FramePrincipal extends javax.swing.JFrame {
 
    private Registro registro = new Registro ();
    private Ingreso  ingreso = new Ingreso();
    
+   public static Sistema sistema = new Sistema();
+   
    
     public FramePrincipal() {
         initComponents();
+        inicializacion();
     }
 
     /**
@@ -148,6 +148,47 @@ public class FramePrincipal extends javax.swing.JFrame {
             }
         });
     }
+    public void inicializacion() {
+        
+         ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+  
+         
+         Usuario a = new Usuario();
+         Usuario b = new Usuario();
+         Usuario c = new Usuario();
+         
+         a.setNombre("juan");
+         a.setPassword("1234");
+         b.setNombre("pedro");
+         b.setPassword("123");
+         c.setNombre("maria");
+         c.setPassword("12345");
+         
+         usuarios.add(a);
+         usuarios.add(b);
+         usuarios.add(c);
+         
+         
+         
+         sistema.setUsuarios(usuarios);
+         
+         
+         
+         
+         
+         
+            for (Usuario u:sistema.getUsuarios()){
+                System.out.println(u.getNombre());
+                System.out.println(u.getPassword());
+                System.out.println("------------");
+            }
+   
+         
+    }
+    
+    
+
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton ingresoB;
